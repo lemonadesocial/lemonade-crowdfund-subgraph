29002994d1ffd4ab23ee18b0963d1ec9112bb1fd
Adenhall <jamesadenhall@gmail.com>
2024-03-07 11:59:15 +0700

    refactor: ensure consistent code

--- src/crowdfund-v1.ts
+++ src/crowdfund-v1.ts
@@ -7,53 +7,65 @@ import {
 } from "../generated/CrowdfundV1/CrowdfundV1"
 import { Assignment, Campaign, CampaignFund, CampaignContributor } from "../generated/schema"
 
-enum State {
-  PENDING,
-  EXECUTED,
-  CONFIRMED,
-  REFUNDED
-}
+const StateMap = [
+  'PENDING',
+  'EXECUTED',
+  'CONFIRMED',
+  'REFUNDED'
+]
 
 export function handleCreate(event: CreateEvent): void {
-  let entity = new Campaign(
+  const campaign = new Campaign(
     event.address.toHexString() + '_' + event.params.campaignId.toString()
   )
 
-  entity.creator = event.params.creator
-  entity.title = event.params.title
-  entity.description = event.params.description
-  entity.state = 'PENDING'
-  entity.totalFunded = BigInt.fromI32(0)
+  campaign.creator = event.params.creator
+  campaign.title = event.params.title
+  campaign.description = event.params.description
+  campaign.state = 'PENDING'
+  campaign.totalFunded = BigInt.fromI32(0)
+  campaign.blockNumber = event.block.number
+  campaign.blockTimestamp = event.block.timestamp
+  campaign.transactionHash = event.transaction.hash
 
-  let assignments = event.params.assignments
+  campaign.save()
+
+  const assignments = event.params.assignments
   for (let i = 0; i < assignments.length; i++) {
-    let assignment = new Assignment(i.toString())
-    assignment.campaign = entity.id
+    const assignment = new Assignment(campaign.id + '_' + i.toString())
+    assignment.campaign = campaign.id
     assignment.to = assignments[i].to
     assignment.count = assignments[i].count
 
     assignment.save()
   }
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
 }
 
 export function handleFund(event: FundEvent): void {
-  let entity = new CampaignFund(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  let campaign = Campaign.load(
+  const campaign = Campaign.load(
     event.address.toHexString() + '_' + event.params.campaignId.toString()
   )
-  let campaignContributor = CampaignContributor.load(event.params.contributor)
 
-  if (!campaign) return;
+  if (!campaign) return
 
   campaign.totalFunded = campaign.totalFunded.plus(event.params.amount)
+
+  campaign.save()
+  
+  const campaignFund = new CampaignFund(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+
+  campaignFund.campaign = event.params.campaignId.toString()
+  campaignFund.amount = event.params.amount
+  campaignFund.contributor = event.params.contributor
+  campaignFund.blockNumber = event.block.number
+  campaignFund.blockTimestamp = event.block.timestamp
+  campaignFund.transactionHash = event.transaction.hash
+
+  campaignFund.save()
+
+  let campaignContributor = CampaignContributor.load(event.params.contributor)
 
   if (campaignContributor) {
     campaignContributor.counter += 1
@@ -63,41 +75,17 @@ export function handleFund(event: FundEvent): void {
     campaignContributor.counter = 1
   }
 
-  entity.campaign = event.params.campaignId.toString()
-  entity.amount = event.params.amount
-  entity.contributor = event.params.contributor
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-  campaign.save()
   campaignContributor.save()
 }
 
 export function handleStateChanged(event: StateChangedEvent): void {
-  let entity = Campaign.load(
+  const campaign = Campaign.load(
     event.address.toHexString() + '_' + event.params.campaignId.toString()
   )
 
-  if (!entity) return;
+  if (!campaign) return
 
-  const state = event.params.state
+  campaign.state = StateMap[event.params.state]
 
-  switch (state) {
-    case State.EXECUTED:
-      entity.state = 'EXECUTED'
-      break;
-    case State.REFUNDED:
-      entity.state = 'REFUNDED'
-      break;
-    case State.CONFIRMED:
-      entity.state = 'CONFIRMED'
-      break;
-    default:
-      break;
-  }
-
-  entity.save()
+  campaign.save()
 }
